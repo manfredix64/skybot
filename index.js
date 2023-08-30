eebba9a7780aa4535e691aeefd339af69465239e
manfredix64 <60573289+manfredix64@users.noreply.github.com>
2023-08-30 17:16:33 +0300

    command handler

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const { Client, Intents } = require("discord.js");
+const fs = require('fs');
+const { Client, Intents, Collection } = require("discord.js");
 const { token, prefix } = require('./config.json');
 
 const client = new Client({
     intents: ['Guilds', 'GuildMessages', 'GuildMembers']
 })
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
     console.log('good')
 })
+
+client.on('messageCreate', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
 
 client.login(token)
